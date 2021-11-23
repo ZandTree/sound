932b6fcbd2fc4badb0efa1020a67265708b90f28
Tatjana <diafox2015@gmail.com>
2021-11-23 12:20:27 +0100

    Update tasks.py

--- contacts/tasks.py
+++ contacts/tasks.py
@@ -7,5 +7,5 @@ User = get_user_model()
 
 @shared_task
 def send_mail_user():
-    send_mail('sweat dreams tonight','main email content for tata','tata-admin@mail.com',['alexriet2001@gmail.com']) 
+    send_mail('sweat dreams tonight','main email content ','tata-admin@mail.com',['uuuuu@gmail.com']) 
     return "mail has been sent"
